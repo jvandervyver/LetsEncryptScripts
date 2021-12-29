669256953c9886f8c145358b5956af50e4ca5f51
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2021-12-29 23:42:46 +0200

    Add LetsEncrypt update script for X11SCL-IF

---
+++ SuperMicro/X11SCL-IF/update_script.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash
+
+# ===============> How to:
+# Put this script 'update_script.sh' in the directory where the cert.pem & privkey.pem is written by LetsEncrypt
+# Ensure it has execute permissions by the LetsEncrypt process
+# Whenever LetsEncrypt updates the certificate the script will be executed by LetsEncrypt
+# Update the configuration variables below
+
+# Configuration
+
+IPMI_HOSTNAME='<HOSTNAME>'
+USERNAME='<USERNAME>'
+PASSWORD='<PASSWORD>'
+
+# Validation
+
+curl "https://${IPMI_HOSTNAME}/" -vvvvv -skSL -X GET 1>/dev/null 2>/dev/null
+
+if [ $? -ne 0 ]; then
+  echo "Could not connecto to https://${IPMI_HOSTNAME} using cURL."
+  echo 'Is cURL installed and the hostname correct?'
+  exit 1
+fi
+
+printf 'test' | base64 1>/dev/null 2>/dev/null
+
+if [ $? -ne 0 ]; then
+  echo 'Could not base64 encode a String, is base64 installed?'
+  exit 1
+fi
+
+printf 'hi' | grep 'hi' 1>/dev/null 2>/dev/null
+
+if [ $? -ne 0 ]; then
+  echo 'Could not grep, is grep installed?'
+  exit 1
+fi
+
+COOKIES_FILE=`mktemp`
+
+if [ $? -ne 0 ]; then
+  echo 'Could not create temporary directory for cookie jar, is mktemp installed?'
+  exit 1
+fi
+
+CONTENT_FILE=`mktemp`
+
+# Ensure cleanup after script execution regardless of outcome
+function post_script_cleanup {
+  rm -rf "${COOKIES_FILE}" 1>/dev/null 2>/dev/null
+  rm -rf "${CONTENT_FILE}" 1>/dev/null 2>/dev/null
+}
+
+trap post_script_cleanup INT TERM EXIT
+
+touch "${COOKIES_FILE}" && touch "${CONTENT_FILE}"
+
+function getCSRFToken() {
+  RESULT=`grep 'CSRF_TOKEN' "${CONTENT_FILE}" | grep -oP 'CSRF_TOKEN",\s*"([^"]*)"\);' | grep -oP '(\,\s?"[^"]*")' | grep -oP '("[^"]*")' | cut -d '"' -f 2`
+  printf "$RESULT"
+}
+
+function base64UrlEncode() {
+  printf "$1" | base64 | sed 's/+/-/g; s,/,_,g'
+}
+
+function urlEncode() {
+  printf "$1" | curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" | cut -c 3-
+}
+
+# Runtime variables
+
+LETS_ENCRYPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+CERT_FILE="/${LETS_ENCRYPT_PATH}/cert.pem"
+
+ls "$CERT_FILE" 1>/dev/null 2>/dev/null
+
+if [ $? -ne 0 ]; then
+  echo "Could not find the LetsEncrypt cert file @ '$CERT_FILE'"
+  exit 1
+fi
+
+KEY_FILE="/${LETS_ENCRYPT_PATH}/privkey.pem"
+
+CSRF_TOKEN=''
+
+ENCODED_USERNAME=`base64UrlEncode "$USERNAME"`
+ENCODED_USERNAME=`urlEncode "$ENCODED_USERNAME"`
+
+ENCODED_PASSWORD=`base64UrlEncode "$PASSWORD"`
+ENCODED_PASSWORD=`urlEncode "$ENCODED_PASSWORD"`
+
+echo '' && echo '==========> Login to BMC:'
+
+curl "https://${IPMI_HOSTNAME}/cgi/login.cgi" -vvvvv -skSL -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data-raw "name=$ENCODED_USERNAME&pwd=$ENCODED_PASSWORD&check=00" -b "${COOKIES_FILE}" -c "${COOKIES_FILE}" -o "${CONTENT_FILE}"
+cat "${CONTENT_FILE}"
+
+sleep 1
+
+echo '' && echo '' && echo '==========> Get CSRF_TOKEN:'
+
+curl "https://${IPMI_HOSTNAME}/cgi/url_redirect.cgi?url_name=topmenu" -vvvvv -skSL -X GET -b "${COOKIES_FILE}" -c "${COOKIES_FILE}" -o "${CONTENT_FILE}"
+CSRF_TOKEN=`getCSRFToken`
+cat "${CONTENT_FILE}"
+
+sleep 1
+
+echo '' && echo '' && echo '==========> Upload SSL certificate and private key:'
+
+curl "https://${IPMI_HOSTNAME}/cgi/upload_ssl.cgi" -vvvvv -skSL -X POST -H 'Expect:' -H "Origin: https://${IPMI_HOSTNAME}" -H "Referer: https://${IPMI_HOSTNAME}/cgi/url_redirect.cgi?url_name=topmenu" -H 'Content-Type: multipart/form-data' -F 'CSRF_TOKEN'="${CSRF_TOKEN}" -F 'cert_file'=@"${CERT_FILE}" -F 'key_file'=@"${KEY_FILE}" -b "${COOKIES_FILE}" -c "${COOKIES_FILE}" -o "${CONTENT_FILE}"
+CSRF_TOKEN=`getCSRFToken`
+cat "${CONTENT_FILE}"
+
+sleep 1
+
+echo '' && echo '' && echo '==========> Approve the uploaded SSL certificate and private key:'
+
+curl "https://${IPMI_HOSTNAME}/cgi/ipmi.cgi" -vvvvv -skSL -X POST -H "Origin: https://${IPMI_HOSTNAME}" -H "Referer: https://${IPMI_HOSTNAME}/cgi/upload_ssl.cgi" -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H "CSRF_TOKEN: ${CSRF_TOKEN}" --data-raw 'op=SSL_VALIDATE.XML&r=(0%2C0)&_=' -b "${COOKIES_FILE}" -c "${COOKIES_FILE}" -o "${CONTENT_FILE}"
+cat "${CONTENT_FILE}"
+
+sleep 1
+
+echo '' && echo '' && echo '==========> Reboot the BMC:'
+
+curl "https://${IPMI_HOSTNAME}/cgi/op.cgi" -vvvvv -skSL -X POST -H "Origin: https://${IPMI_HOSTNAME}" -H "Referer: https://${IPMI_HOSTNAME}/cgi/upload_ssl.cgi" -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8'  -H "CSRF_TOKEN: ${CSRF_TOKEN}" --data-raw 'op=main_bmcreset&_=' -b "${COOKIES_FILE}" -c "${COOKIES_FILE}" -o "${CONTENT_FILE}"
+cat "${CONTENT_FILE}"
+
+echo ''
